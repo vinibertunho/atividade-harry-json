d624e8cad825ba3c2e15d8fa267042265da89eff
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-08-18 13:25:00 -0300

    levante o servidor

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bruxos from "./bruxos.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bem-vindo ao Mundo de Harry Potter!</h1>");
+});
+
+app.get("/bruxos", (req, res) => {
+  res.json(bruxos);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`🧙‍♂️ API dos Bruxos está no ar na porta  http://localhost:${PORT}`);
+});
